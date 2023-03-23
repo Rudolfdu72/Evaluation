4ef26d5e9c2d4f34917db2e3028e1e404fc356d0
Rudolf Apoto <rudolf.pro27@gmail.com>
2023-03-23 17:10:53 +0100

    création des contenus de la page d'accueil

---
+++ script.js
@@ -0,0 +1,26 @@
+// let lid = document.querySelectorAll('.nav-link');
+// // console.log(lid)
+
+// lid.forEach((element)=>{
+//   element.addEventListener("mouseover", function( event ) {
+//     element.style.color = "green";
+  
+//     });
+
+//     element.addEventListener("mouseout", function( event ) {
+//       element.style.color = "";
+//     });
+// })
+
+  // $(()=>{
+  //   $('lid').mouseover(function (event) { 
+  //     event.target.style.background ='red'
+     
+  
+  //   });
+    
+  //     $('lid').mouseout(function (event) { 
+  //       event.target.style.background =''
+       
+  //     });
+  // })
